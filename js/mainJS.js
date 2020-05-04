8d68415a5244966892cbcc9dd03c4b76488f0ef4
Yaroslav <55097486+g0dDamnR1ght@users.noreply.github.com>
2020-05-04 14:12:37 +0300

    change card model
    изменена модель карточки, которая должна отрисовываться.

--- js/mainJS.js
+++ js/mainJS.js
@@ -52,16 +52,14 @@ function renderCard() {
 }
 
 function getTamplate(card) {
-  const cardTemplate = ` <div class="card">
-    <div class="card-header">
-      ${card.task}
-    </div>
-    <div class="card-body">
-      <blockquote class="blockquote mb-0">
-        <p>${card.description}</p>
-      </blockquote>
-    </div>
-  </div>` 
+  const cardTemplate = `<div class="card w-75">
+        <div class="card-body">
+          <h5 class="card-title">${card.task}</h5>
+          <p class="card-text">${card.description}</p>
+          <button class="delete_Task"><i class="fas fa-minus-circle"></i></button>
+          <button class="edit_Task"><i class="fas fa-edit"></i></button>
+        </div>
+      </div> ` 
 
   return cardTemplate
 }
