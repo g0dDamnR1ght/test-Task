28fb2f54424c5ceb43e602605c3f9ec0f3736205
Yaroslav <55097486+g0dDamnR1ght@users.noreply.github.com>
2020-05-05 14:23:35 +0300

    add save function, corrected delete method
    Исправил костыль, который был в методе удаления.
    добавил сохранение файлов в local storage.

--- js/mainJS.js
+++ js/mainJS.js
@@ -3,8 +3,7 @@ const descriptionInp = document.querySelector('.inp_description');
 const taskInput = document.querySelector('.inp_text'); 
 const addCard =  document.querySelector('.card_add');
 const cardGroup = document.querySelector('.card-deck'); 
-
-
+ 
 let cardList = [
   {
     description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.`,
@@ -26,6 +25,10 @@ let cardList = [
   },
 ]; 
 
+// ===========localStorage function============
+const toLS = (item, key) => localStorage.setItem(key, JSON.stringify(item)); 
+
+// ============================================
 
 addCard.addEventListener('click', submitForm); 
 
@@ -39,6 +42,7 @@ function submitForm(e) {
     createCard(descriptionInp.value, taskInput.value); 
     descriptionInp.value = ""; 
     taskInput.value = ""; 
+    // toLS('arr', cardList)
   }
 }
 
@@ -52,9 +56,10 @@ function createCard (description, task) {
 
   cardList.push(card); 
   console.log(cardList)
+  toLS(cardList, "arr" )
 
   let div = document.createElement('div'); 
-  div.setAttribute('class', 'col mb-4 main'); 
+  div.setAttribute('class', 'col-3 mb-4 main'); 
   div.innerHTML = getTamplate(card); 
   cardGroup.appendChild(div)
   getButton(div)
@@ -62,14 +67,23 @@ function createCard (description, task) {
 }
 
 function renderCards() {
- cardList.map( c => {
+  let get = localStorage.getItem("arr"); 
+  let data = JSON.parse(get)
+
+  cardList = data
+  
+if(!data || data === null){
+   emptyTemplate() 
+}else {
+  data.map( c => {
   let div = document.createElement('div'); 
-  div.setAttribute('class', 'col-3 mb-4 align-self-center'); 
+  div.setAttribute('class', 'col-3 mb-4  main'); 
   div.innerHTML = getTamplate(c); 
   cardGroup.appendChild(div); 
   getButton(div)
  getEditButton(div)
-})
+    })
+  }
 }
 
 function getTamplate(card) {
@@ -79,10 +93,21 @@ function getTamplate(card) {
           <h5 class="card-title" id="card-title">${card.task}</h5>
           <p class="card-text" id="card-text">${card.description}</p>
           <button class="delete_Task" data-id="${card.id}"><i class="fas fa-minus-circle fa-2x"></i></button>
-          <button class="edit_Task" data-edit="${card.isEdit}"><i class="fas fa-edit fa-2x"></i></button>
+          <button class="edit_Task" data-edit="${card.isEdit}"> <i class="fas fa-edit fa-2x"></i></button>
         </div>
       </div> ` 
 
+  return cardTemplate
+}
+
+function emptyTemplate() {
+  const cardTemplate = `
+         <div class="card">
+          <div class="card-body">
+            <h3 class="card-title" id="card-title">У Вас еще нет дел!</h3>
+          </div>
+        </div> 
+  `
   return cardTemplate
 }
 
@@ -99,60 +124,52 @@ function getEditButton(card) {
    changeBtn.forEach( b => b.addEventListener('click', changeHandler))
 }
 
-// тут костыль, его поправить нужно
-
 function deleteCard(event) {
  const b = event.target; 
  if(b.hasAttribute("data-id")){
-  let id = b.parentNode.parentNode.dataset; 
+  let id = b.closest('.delete_Task').dataset; 
    cardList = cardList.filter(c => c.id !== +id.id)
-  b.parentNode.parentNode.parentNode.remove()
+  b.closest('.main').remove()
+  toLS(cardList, "arr")
  }else {
    let id = b.parentNode.dataset; 
    cardList = cardList.filter(c => c.id !== +id.id)
-  b.parentNode.parentNode.parentNode.parentNode.remove()
+  b.closest('.main').remove()
+  toLS(cardList, "arr")
  }
+ 
 }
 
-function changeHandler(){
-  changeTask(event); 
-  changeDecription(event); 
+// поставить дизєйбл кнопке
+
+function changeHandler(event){
+let closest = event.target.closest('.card-body');
+let cardTitle = closest.querySelector('.card-title'); 
+let cardDescription = closest.querySelector('.card-text');
+
+
+  changeTask(event, closest, cardTitle); 
+  changeTask(event, closest, cardDescription)
+  
 }
 
-function changeTask(event){
-let closest = event.target.closest('.card-body'); 
-let txt = closest.querySelector('.card-title').childNodes[0].nodeValue;
-
+function changeTask(event, closest, title){
+  let txt = title.childNodes[0].nodeValue;
+  let btnEdit = closest.querySelector('.edit_Task'); 
   let input = document.createElement('input'); 
-  input.setAttribute('type', 'text'); 
+  input.setAttribute('type', 'text');
+  input.setAttribute('class', 'input'); 
   input.setAttribute('value', `${txt}`);
+  btnEdit.setAttribute('disabled', 'disabled')
   closest.appendChild(input)
 
-  input.addEventListener('input', () => closest.querySelector('.card-title').innerHTML = input.value)
+  input.addEventListener('input', () => title.innerHTML = input.value)
   input.addEventListener('focus', () => input.value = "")
   input.addEventListener('change', () => {
-      closest.querySelector('.card-title').innerHTML = input.value
-    input.remove(); 
-  })
-  
+      title.innerHTML = input.value
+      input.remove(); 
+      btnEdit.removeAttribute('disabled')
+  }) 
 }
-
- function changeDecription(event) {
-  let closest = event.target.closest('.card-body'); 
-  let txt = closest.querySelector('.card-text').childNodes[0].nodeValue; 
-
-  let input = document.createElement('input'); 
-  input.setAttribute('type', 'text'); 
-  input.setAttribute('value', `${txt}`)
-  closest.appendChild(input)
-  
-  input.addEventListener('input', () => closest.querySelector('.card-text').innerHTML = input.value)
-  input.addEventListener('focus', () => input.value = "")
-  input.addEventListener('change', () => {
-    closest.querySelector('.card-text').innerHTML = input.value
-    input.remove(); 
-  })
-}
-
 
 renderCards(); 
